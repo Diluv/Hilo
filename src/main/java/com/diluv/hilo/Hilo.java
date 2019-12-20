e8320759f1e18421303ab8d36797836d75f75fdb
Tyler Hancock <darklime@live.ca>
2019-12-20 05:12:29 -0700

    Added interface and rough outline for how processing would work.

--- src/main/java/com/diluv/hilo/Hilo.java
+++ src/main/java/com/diluv/hilo/Hilo.java
@@ -1,8 +1,66 @@
 package com.diluv.hilo;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.logging.log4j.core.Logger;
+
 public class Hilo {
-
-    public static void main (String[] args) {
-
+    
+    /**
+     * An ArrayList which holds all the processing steps for a given processing
+     * procedure. The order of insertion is used as the execution order for the
+     * various processing steps.
+     */
+    private final ArrayList<IProcessStep> processingSteps;
+    
+    /**
+     * A logger instance that can be used by the processor and processing steps
+     * to report debug information and errors.
+     */
+    private final Logger log;
+    
+    public Hilo (Logger log) {
+        
+        this.processingSteps = new ArrayList<>();
+        this.log = log;
+    }
+    
+    public Logger getLogger () {
+        
+        return this.log;
+    }
+    
+    public void processFile (File input) {
+        
+        final UUID processId = UUID.randomUUID();
+        final File workingDir = new File(processId.toString());
+        
+        try {
+            
+            Files.createDirectories(workingDir.toPath());
+        }
+        
+        catch (IOException e) {
+            
+            this.getLogger().error("Failed to create the working directory {}.", workingDir.getPath());
+            this.getLogger().catching(e);
+            return;
+        }
+        
+        Map<String, Object> properties = new HashMap<>();
+        
+        for (IProcessStep step : this.processingSteps) {
+            
+            if (step.validate(this, processId, input)) {
+                
+                step.process(this, processId, workingDir, input, properties);
+            }
+        }
     }
 }
---
+++ src/main/java/com/diluv/hilo/IProcessStep.java
@@ -0,0 +1,49 @@
+package com.diluv.hilo;
+
+import java.io.File;
+import java.util.Map;
+import java.util.UUID;
+
+public interface IProcessStep {
+    
+    /**
+     * Performs a processing step on a file as part of a larger processing
+     * pipeline.
+     * 
+     * @param hilo The instance of Hilo which invoked the processing step.
+     * @param processID An identifier for the current processing operation that
+     *        is unique for each file processed.
+     * @param workingDir The current working directory for the current
+     *        processing operation. This is created before the file is processed
+     *        and will be deleted after all process steps have been completed.
+     * @param file The file being processed.
+     * @param properties A map of properties read from the file. //TODO replace
+     *        with a databse connection.
+     */
+    void process (Hilo hilo, UUID processID, File workingDir, File file, Map<String, Object> properties);
+    
+    /**
+     * Gets a name for the process. This is used as part of the logger to
+     * describe the current processing step stage.
+     * 
+     * @return The name of the process.
+     */
+    String getProcessName ();
+    
+    /**
+     * Validates whether or not a process can be ran on a given file. If this
+     * check fails processor will simply continue to the next step. If an
+     * exception is thrown processing will stop and the file will fail it's
+     * processing check.
+     * 
+     * @param hilo The instance of hilo which invoked the processing step.
+     * @param processID An identifier for the current processing operation that
+     *        is unique for each file processed.
+     * @param file The file being processed.
+     * @return Whether or not the process step can run for the given file.
+     *         Returning false will simply tell the processor to skip to the
+     *         next step. If an exception is thrown processing will stop and the
+     *         file will fail it's processing check.
+     */
+    boolean validate (Hilo hilo, UUID processID, File file);
+}
