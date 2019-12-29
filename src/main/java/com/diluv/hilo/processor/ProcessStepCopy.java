a333775e511f0cc788e6adaf66b5e129f0e90a28
Tyler Hancock <darklime@live.ca>
2019-12-29 01:23:55 -0700

    Created a simple process step to copy original file to the working directory.

---
+++ src/main/java/com/diluv/hilo/processor/ProcessStepCopy.java
@@ -0,0 +1,42 @@
+package com.diluv.hilo.processor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+
+import com.diluv.hilo.Hilo;
+
+public class ProcessStepCopy implements IProcessStep {
+
+    private static final String NAME = "Copy Original To Working Directory";
+
+    @Override
+    public void process (Hilo hilo, UUID processID, File workingDir, File file, Map<String, Object> properties) {
+
+        try {
+
+            FileUtils.copyFileToDirectory(file, workingDir);
+        }
+
+        catch (final IOException e) {
+
+            hilo.getLogger().error("Failed to copy file with id {} to working directory.", processID);
+            hilo.getLogger().catching(e);
+        }
+    }
+
+    @Override
+    public String getProcessName () {
+
+        return NAME;
+    }
+
+    @Override
+    public boolean validate (Hilo hilo, UUID processID, File file) {
+
+        return file.exists();
+    }
+}
