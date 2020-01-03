be62ef761053895fec1133b5bbf51eca98d0b2c7
Tyler Hancock <darklime@live.ca>
2020-01-03 15:22:30 -0700

    Rewrote hash processing step to only include SHA 512 using Apache codec.

--- src/main/java/com/diluv/hilo/processor/ProcessStepHash.java
+++ src/main/java/com/diluv/hilo/processor/ProcessStepHash.java
@@ -3,50 +3,40 @@ package com.diluv.hilo.processor;
 import java.io.BufferedInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.MessageDigest;
 
-import javax.xml.bind.DatatypeConverter;
-
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.logging.log4j.Logger;
 
 import com.diluv.hilo.procedure.FileData;
 
 /**
- * This processing step collects common hashes for the file.
+ * This processing step is responsible for calculating the SHA 512 hash of a file.
  */
 public class ProcessStepHash implements IProcessStep {
 
+    /**
+     * The name of the processing step as a constant value.
+     */
+    private static final String NAME = "Calculating SHA 512 Hash";
+    
+    /**
+     * A global instance of this processing step.
+     */
     public static final IProcessStep INSTANCE = new ProcessStepHash();
 
     @Override
     public void process (Logger log, FileData data, Path workingDir, Path file) throws Exception {
 
-        final MessageDigest md5 = MessageDigest.getInstance("MD5");
-        final MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-        final MessageDigest sha512 = MessageDigest.getInstance("SHA-512");
-
-        final byte[] buffer = new byte[8192];
-        int count;
-
         try (BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(file))) {
 
-            while ((count = bis.read(buffer)) > 0) {
-
-                md5.update(buffer, 0, count);
-                sha256.update(buffer, 0, count);
-                sha512.update(buffer, 0, count);
-            }
-
-            data.md5 = DatatypeConverter.printHexBinary(md5.digest()).toLowerCase();
-            data.sha256 = DatatypeConverter.printHexBinary(sha256.digest()).toLowerCase();
-            data.sha512 = DatatypeConverter.printHexBinary(sha512.digest()).toLowerCase();
+            data.sha512 = DigestUtils.sha256Hex(bis);
         }
     }
 
     @Override
     public String getProcessName () {
 
-        return "Collecting Hashes";
+        return NAME;
     }
 
     @Override
