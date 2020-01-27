eccee829818d7db96304be44bb7c9ec8d28b48d1
lclc98 <lclc98@lclc98.com>
2020-01-27 21:02:17 +1000

    Removed database files

--- src/main/resources/db/query/file_queue/findOneWherePending.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM project_file_queue
-WHERE status = 'pending'
-ORDER BY created_at
-LIMIT ?;
--- src/main/resources/db/query/file_queue/updateStatusById.sql
+++
@@ -1,4 +0,0 @@
-UPDATE project_file_queue
-SET status             = 'running',
-    status_change_time = NOW()
-WHERE id = ?;
